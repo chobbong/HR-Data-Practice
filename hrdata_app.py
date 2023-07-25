15d57cc4788622283ed0a5e11210c02832e3e984
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-07-25 17:15:09 +0900

    add shap

--- hrdata_app.py
+++ hrdata_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
-import shap
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets
-from sklearn.ensemble import RandomForestRegressor
+import shap
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
